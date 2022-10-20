6657a7da0eb44035a1c08e3eb11e14ec7ff5d2da
christianrhodes18 <thechristianrhodes@gmail.com>
2022-10-20 16:16:41 -0400

    Initial Commit

---
+++ gift-tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gift-tracker
+//
+//  Created by Christian Rhodes on 10/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gift-tracker/gift_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gift_trackerApp.swift
+//  gift-tracker
+//
+//  Created by Christian Rhodes on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct gift_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
